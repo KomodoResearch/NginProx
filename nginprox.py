6e5a337a4a3791ca9ef642460621c2b4ba10a9a3
razmag <razmagorii@gmail.com>
2019-01-27 16:46:12 +0200

    Preparing to add nginx auto-install - Proxy positional argument changed to optional (default to 127.0.0.1) - Added -I to argparser - def install_ngn: check if nginx is installed (learned a lot about subprocess and Popen)

--- nginprox.py
+++ nginprox.py
@@ -1,11 +1,16 @@
 import os
 # import sys
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument('proxy', help='IP to proxy to (e.g. "example.com" / "192.168.1.2" )')
-parser.add_argument('-r', '--root-folder', help="Specify the web-server's root directory (e.g /var/www/html), "
-                                                "by default using current directory", default=os.getcwd())
+parser.add_argument('-p', '--proxy', default='http://127.0.0.1', help='IP to proxy to (e.g. "example.com" / '
+                                                                      '"192.168.1.2" ) on default using localhost')
+parser.add_argument('-r', '--root-folder', default=os.getcwd(), help="Specify the web-server's root directory (e.g "
+                                                                     "/var/www/html), by default using current "
+                                                                     "directory")
+parser.add_argument('-I', '--install', action='store_true', help="Use -I to install the nginx and set it up with "
+                                                                 "reverse proxy")
 args = parser.parse_args()
 if not args.proxy.startswith('http://'):  # use the first parameter as the ip
     ip = 'http://' + args.proxy
@@ -28,7 +33,7 @@ for root, dirs, files in os.walk(cwd):  # traverse directories
         # don't create a listing for the script, defaults file or the .conf file
         if file != 'nginprox.py' and file != 'defaults.txt' and file != 'nginx.conf':
             pPath = ((root.replace(cwd, '') + os.sep + file).replace('\\', '/'))  # define the pPath in the right syntax
-            conf.write("\t\tlocation = {0} {{\n\t\t\t proxy_pass {1};  \n\t\t}}\n".format(pPath, ip+pPath))
+            conf.write("\t\tlocation = {0} {{\n\t\t\t proxy_pass {1};  \n\t\t}}\n".format(pPath, ip + pPath))
 print("Creating exceptions")
 # RegEx to match allowed files #TODO make this better
 conf.write("\t\tlocation ~* .*\\.(gif|png|jp.g)$ {{\n\t\t\t proxy_pass {0};\n\t\t}}\n".format(ip))
@@ -37,3 +42,16 @@ print("Appending a 'catch-all'")
 conf.write('\t\tlocation ~* .*$ {{\n\t\t\trewrite .* /404 break;\n\t\t\tproxy_pass {0};\n\t\t}}\n'.format(ip))
 conf.write("\t}\n}")  # end the http and server open brackets
 print("finished :)")
+
+
+def install_ngn():
+    """Installs the Nginx service and checks that it is active"""
+    systemctl = ['systemctl', '-l', '--type', 'service', '--all']  # command to list all services on the device
+    grep = ['grep', 'nginx']  # command to grep for nginx
+    systemctl_process = subprocess.Popen(systemctl, shell='true', stdout=subprocess.PIPE)  # run the systemctl command
+    grep_process = subprocess.Popen(grep, stdin=systemctl_process.stdout, universal_newlines='true',
+                                    stdout=subprocess.PIPE)  # run the grep command on the previous command
+    output = grep_process.communicate()  # grab output
+    if output is not None:  # TODO install nginx
+    else:
+        return False  # use as flag for "is nginx already installed"
