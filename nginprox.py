c6f8f76e9f2650610407de1e4823e670be2c8d77
razmag <razmagorii@gmail.com>
2019-02-06 12:29:06 +0200

    moved everything to a function

--- nginprox.py
+++ nginprox.py
@@ -45,53 +45,76 @@ def ngn_bind_port(port="8888"):
     subprocess.Popen(restart, stdout=subprocess.PIPE)  # restart nginx
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--proxy', default='http://127.0.0.1', help='IP to proxy to (e.g. "example.com" / '
-                                                                      '"192.168.1.2" ) on default using localhost')
-parser.add_argument('-r', '--root-folder', default=os.getcwd(), help="Specify the web-server's root directory (e.g "
-                                                                     "/var/www/html), by default using current "
-                                                                     "directory")
-parser.add_argument('-i', '--install', action='store_true', help="Use -I to install the nginx and set it up with "
-                                                                 "reverse proxy")
-args = parser.parse_args()
-if not args.proxy.startswith('http://'):  # use the first parameter as the ip
-    ip = 'http://' + args.proxy
-else:
-    ip = args.proxy
-cwd = args.root_folder  # set the root folder to the arg parsed
-if args.install:
-    print("Checking if Nginx is already installed")
-    if not ngn_check():
-        print("Installing Nginx")
-        ngn_install()
+def argparser():
+    """argument parser"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--proxy', default='http://127.0.0.1', help='IP to proxy to (e.g. "example.com" / '
+                                                                          '"192.168.1.2" ) on default using localhost')
+    parser.add_argument('-r', '--root-folder', default=os.getcwd(), help="Specify the web-server's root directory (e.g "
+                                                                         "/var/www/html), by default using current "
+                                                                         "directory")
+    parser.add_argument('-i', '--install', action='store_true', help="Use -I to install the nginx and set it up with "
+                                                                     "reverse proxy")
+    return_args = parser.parse_args()
+    return return_args
+
+
+def write_defaults(output):
+    """copy content of default file into conf file"""
+    print("Writing defaults to main context")
+    defaults = open("defaults")  # copy nginx start of settings file into conf
+    for line in defaults:  # copy defaults into .conf file
+        output.write(line)
+    defaults.close()
+    return
+
+
+def proxy_dedicator(output, ip, cwd):
+    """main proxy function"""
+    print("Traversing paths and dedicating proxy")
+    output.write('\t\tlocation ~* ^/$ {{\n\t\t\trewrite .* / break;\n\t\t\tproxy_pass {0};\n\t\t}}\n'.format(ip))  #
+    # route main IP to main site IP
+    for root, dirs, files in os.walk(cwd):  # traverse directories
+        for file in files:
+            # don't create a listing for the script, defaults file or the .conf file
+            if file != 'nginprox.py' and file != 'defaults.txt' and file != 'nginx.conf':
+                pPath = (
+                    (root.replace(cwd, '') + os.sep + file).replace('\\', '/'))  # define the pPath in the right syntax
+                output.write("\t\tlocation = {0} {{\n\t\t\t proxy_pass {1};  \n\t\t}}\n".format(pPath, ip + pPath))
+    print("Creating exceptions")
+    # RegEx to match allowed files #TODO change this to a customizable function
+    output.write("\t\tlocation ~* .*\\.(gif|png|jp.g)$ {{\n\t\t\t proxy_pass {0};\n\t\t}}\n".format(ip))
+    print("Appending a 'catch-all'")
+    # catch all, throw to 404
+    output.write('\t\tlocation ~* .*$ {{\n\t\t\trewrite .* /404 break;\n\t\t\tproxy_pass {0};\n\t\t}}\n'.format(ip))
+    output.write("\t}\n}")  # end the http and server open brackets
+
+
+def main():
+    args = argparser()
+    if not args.proxy.startswith('http://'):  # use the first parameter as the ip
+        ip = 'http://' + args.proxy
     else:
-        input("Nginx already installed, press enter to create .conf file and attach it to existing nginx installation")
-    ngn_bind_port()  # change port to 8888 on default so nginx wont crash when clashing with existing web server
-pPath = ''  # path to proxy to
-print("Creating nginx.conf file")
-conf = open('nginx.conf', 'w+')  # create the .conf file
-print("Writing defaults to main context")
-defaults = open("defaults")  # copy nginx start of settings file into conf
-for line in defaults:  # copy defaults into .conf file
-    conf.write(line)
-defaults.close()
-print("Traversing paths and dedicating proxy")
-conf.write('\t\tlocation ~* ^/$ {{\n\t\t\trewrite .* / break;\n\t\t\tproxy_pass {0};\n\t\t}}\n'.format(ip))  #
-# route main IP to main site IP
-for root, dirs, files in os.walk(cwd):  # traverse directories
-    for file in files:
-        # don't create a listing for the script, defaults file or the .conf file
-        if file != 'nginprox.py' and file != 'defaults.txt' and file != 'nginx.conf':
-            pPath = ((root.replace(cwd, '') + os.sep + file).replace('\\', '/'))  # define the pPath in the right syntax
-            conf.write("\t\tlocation = {0} {{\n\t\t\t proxy_pass {1};  \n\t\t}}\n".format(pPath, ip + pPath))
-print("Creating exceptions")
-# RegEx to match allowed files #TODO make this better
-conf.write("\t\tlocation ~* .*\\.(gif|png|jp.g)$ {{\n\t\t\t proxy_pass {0};\n\t\t}}\n".format(ip))
-print("Appending a 'catch-all'")
-# catch all, throw to 404
-conf.write('\t\tlocation ~* .*$ {{\n\t\t\trewrite .* /404 break;\n\t\t\tproxy_pass {0};\n\t\t}}\n'.format(ip))
-conf.write("\t}\n}")  # end the http and server open brackets
-if args.install:
-    print("copying .conf file to nginx directory")
-    subprocess.Popen("sudo cp nginx.conf /etc/nginx/nginx.conf".split(), stdout=subprocess.PIPE)
-print("finished :)")
+        ip = args.proxy
+    cwd = args.root_folder  # set the root folder to the arg parsed
+    if args.install:
+        print("Checking if Nginx is already installed")
+        if not ngn_check():
+            print("Installing Nginx")
+            ngn_install()
+        else:
+            input(
+                "Nginx already installed, press enter to create .conf file and attach it to existing nginx installation")
+        ngn_bind_port()  # change port to 8888 on default so nginx wont crash when clashing with existing web server
+    print("Creating nginx.conf file")
+    conf = open('nginx.conf', 'w+')
+    write_defaults(conf)
+    proxy_dedicator(conf, ip, cwd)
+    if args.install:
+        print("copying .conf file to nginx directory")
+        subprocess.Popen("sudo cp nginx.conf /etc/nginx/nginx.conf".split(), stdout=subprocess.PIPE)
+    print("finished :)")
+
+
+if __name__ == "__main__":
+    main()
